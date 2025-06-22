eb6dcd6196c88bba4cb1b5be038132de0ea6c682
replakcan <alper.makcan@gmail.com>
2025-06-22 18:39:55 +0300

    Creates shop-page component
    shows all products as default

--- src/routes/shop-page.jsx
+++ src/routes/shop-page.jsx
@@ -1,6 +1,35 @@
-function ShopPage () {
+import "../styles/shop-page.css";
 
-    return <div>Shop Page</div>
-} 
+import Categories from "../components/categories";
+import useProduct from "../hooks/useProduct";
+import { Outlet, useLoaderData } from "react-router-dom";
 
-export default ShopPage
+// eslint-disable-next-line react-refresh/only-export-components
+export async function loader() {
+  const response = await fetch("https://fakestoreapi.com/products/categories", {
+    mode: "cors",
+  });
+
+  if (response.status >= 400) throw new Error("Error occured");
+
+  const categories = await response.json();
+  return categories;
+}
+
+function ShopPage() {
+  const categories = useLoaderData();
+  const { products, error, loading } = useProduct();
+
+  if (error) return <div>Oops, something went wrong!</div>;
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <section className="shop-page">
+      <Categories categories={categories} />
+      <Outlet context={products} />
+    </section>
+  );
+}
+
+export default ShopPage;
