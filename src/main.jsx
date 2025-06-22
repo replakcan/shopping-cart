6b8599a12f026685c4ddfbe0863415b16a08f2ba
replakcan <alper.makcan@gmail.com>
2025-06-22 18:35:39 +0300

    Moves routes from main to a separate file

--- src/main.jsx
+++ src/main.jsx
@@ -2,40 +2,9 @@ import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ShopPage from "./routes/shop-page.jsx";
-import Root from "./routes/root.jsx";
-import ErrorPage from "./routes/error-page.jsx";
-import Index from "./routes/index.jsx";
-import ShoppingCart from "./routes/shopping-cart.jsx";
-import HomePage from "./routes/home-page.jsx";
+import routes from "./routes.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          { index: true, element: <Index /> },
-          {
-            path: "home",
-            element: <HomePage />,
-          },
-          {
-            path: "shop",
-            element: <ShopPage />,
-          },
-          {
-            path: "shopping-cart",
-            element: <ShoppingCart />,
-          },
-        ],
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
