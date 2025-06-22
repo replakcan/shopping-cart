2361a13914484d110173b84b065b6b80a347703e
replakcan <alper.makcan@gmail.com>
2025-06-22 22:05:14 +0300

    Updates shopping-cart with cart-items

--- src/routes/shopping-cart.jsx
+++ src/routes/shopping-cart.jsx
@@ -1,6 +1,22 @@
 import { useOutletContext } from "react-router-dom";
+import ProductCard from "../components/product-card";
+import CartItem from "../components/cart-item";
+
 function ShoppingCart() {
   const { cart } = useOutletContext();
+
+  console.log(Object.values(cart));
+  return (
+    <div>
+      <h2>Items in your cart:</h2>
+      <hr />
+      {cart.length ? (
+        cart.map((item) => <CartItem key={item.product.id} cartItem={item} />)
+      ) : (
+        <div>cart is empty</div>
+      )}
+    </div>
+  );
 }
 
 export default ShoppingCart;
